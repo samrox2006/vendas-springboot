ff182499a15645e374995922b17beb07d58607ec
Bruno Mulin <verdibianco@hotmail.com>
2024-01-27 01:12:41 -0300

    Crud com JpaRepository

--- src/main/java/com/brunosam/vendas/VendasApplication.java
+++ src/main/java/com/brunosam/vendas/VendasApplication.java
@@ -17,31 +17,31 @@ public class VendasApplication {
 	public CommandLineRunner init(@Autowired Clientes clientes){
 		return args -> {
 			System.out.println("Salvando clientes..");
-			clientes.salvar(new Cliente("Douglas"));
-			clientes.salvar(new Cliente("Bruno"));
-			clientes.salvar(new Cliente("Breno"));
+			clientes.save(new Cliente("Douglas"));
+			clientes.save(new Cliente("Bruno"));
+			clientes.save(new Cliente("Breno"));
 
-			List<Cliente> todosClientes = clientes.obterTodos();
+			List<Cliente> todosClientes = clientes.findAll();
 			todosClientes.forEach(System.out::println);
 
 			System.out.println("Atualizando clientes..");
 			todosClientes.forEach(c->{
 				c.setNome(c.getNome()+ " aluatlizado.");
-				clientes.atualizar(c);
+				clientes.save(c);
 			});
 
-			todosClientes = clientes.obterTodos();
+			todosClientes = clientes.findAll();
 			todosClientes.forEach(System.out::println);
 
 			System.out.println("Buscando clientes...");
-			clientes.buscarPorNome("Br").forEach(System.out::println);
+			clientes.findByNomeLike("Br").forEach(System.out::println);
 
 			System.out.println("Deletando clientes...");
-			clientes.obterTodos().forEach(c->{
-				clientes.deletar(c);
+			clientes.findAll().forEach(c->{
+				clientes.delete(c);
 			});
 
-			todosClientes = clientes.obterTodos();
+			todosClientes = clientes.findAll();
 			if(todosClientes.isEmpty()){
 				System.out.println("Nenhum cliente encontrado.");
 			}else{
